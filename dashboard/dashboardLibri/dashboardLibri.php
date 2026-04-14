e85d7fff390982c4a32002434aacdc8f92becd5d
Marcoding04 <marcomezzanotte2004@gmail.com>
2026-04-14 12:46:57 +0200

    piccoli fixes2.0

--- dashboard/dashboardLibri/dashboardLibri.php
+++ dashboard/dashboardLibri/dashboardLibri.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 session_start();
-$root= "../../";
+$root= "../..";
 require_once("../../auth/cookies.php");
 require_once("../../utils/connect.php");
 if ($_SESSION['utenza'] == 1 || $_SESSION['utenza'] == 2) {
@@ -17,7 +17,7 @@ if ($_SESSION['utenza'] == 1 || $_SESSION['utenza'] == 2) {
 if (isset($_POST['createDummy'])) {
 	$isbnDummy = rand(1000000000000,9999999999999);
 	$sql = "INSERT INTO Opera (`ISBN`, `Nome`, `Autore`, `Genere`, `Descrizione`, `Copertina`, `CasaEditrice`, `AnnoPubblicazione`)
-		VALUES ($isbnDummy, 'Dummy', 'Dummy', 'Umoristico', 'DummyDummyDummy', 'img/books/default.png', 'Dummy', 1984)";
+		VALUES ($isbnDummy, 'Dummy', 'Dummy', 'Umoristico', 'DummyDummyDummy',  '../../../img/books/default.jpg', 'Dummy', 1984)";
 
 	$sql2 = "INSERT INTO copiaLibro (`ISBN`, `Stato`) VALUES ($isbnDummy, '1')";
 
