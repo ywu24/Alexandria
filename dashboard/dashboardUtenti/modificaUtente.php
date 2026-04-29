94c92ce72903320f2a561f7a452a3e5cd038cb18
Luca Pitti <lucapitti36@gmail.com>
2026-04-29 13:02:54 +0200

    responsivness

--- dashboard/dashboardUtenti/modificaUtente.php
+++ dashboard/dashboardUtenti/modificaUtente.php
@@ -21,7 +21,6 @@ if (isset($_GET['error'])) {
 
 <head>
 	<title>Gestione Utente</title>
-	<link rel="stylesheet" href="../../css/style.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
